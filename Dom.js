fbbfaf160ebfc544cf6cee5c90c0f0af5ccb6208
pujachitale <chitalepuja99@gmail.com>
2023-05-23 14:03:57 +0530

    first commit

--- Dom.js
+++ Dom.js
@@ -21,32 +21,51 @@
 // console.log(headerTitle.innerText)
 
 // GETELEMENTBYCLASSNAME
-var items=document.getElementsByClassName('list-group-item')
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello 2'
-items[1].style.fontWeight='bold'
-items[1].style.backgroundColor='yellow'
+// var items=document.getElementsByClassName('list-group-item')
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2'
+// items[1].style.fontWeight='bold'
+// items[1].style.backgroundColor='yellow'
 
-for(var i=0;i< items.length; i++){
-    items[i].style.backgroundColor='#f4f4f4'
+// for(var i=0;i< items.length; i++){
+//     items[i].style.backgroundColor='#f4f4f4'
+// }
+
+
+// // Make the 3 rd element in the list have green background color
+// items[2].style.backgroundColor='green'
+
+// // Make all the elements in the list have bold color font
+// items[0].style.fontWeight='bold'
+// items[1].style.fontWeight='bold'
+// items[2].style.fontWeight='bold'
+// items[3].style.fontWeight='bold'
+
+
+
+var li=document.getElementsByTagName('li')
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello 2'
+li[1].style.fontWeight='bold'
+li[1].style.backgroundColor='yellow'
+
+for(var i=0;i< li.length; i++){
+    li[i].style.backgroundColor='#f4f4f4'
 }
 
 
-// Make the 3 rd element in the list have green background color
-items[2].style.backgroundColor='green'
-
-// Make all the elements in the list have bold color font
-items[0].style.fontWeight='bold'
-items[1].style.fontWeight='bold'
-items[2].style.fontWeight='bold'
-items[3].style.fontWeight='bold'
+// QuerySelector
+var header=document.querySelector('#main-header')
+header.style.borderBottom='solid 4px black'
 
 
+// var input=document.querySelector('input')
+// input.value='Hello world'
 
-
-
-
+// var submit = document.querySelector('input[type="submit"]')
+// submit.value="send"
 
 
 
---
+++ Dom1.js
@@ -0,0 +1,18 @@
+// var li=document.getElementsByTagName('li')
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello 2'
+// li[1].style.fontWeight='bold'
+// li[1].style.backgroundColor='yellow'
+
+// for(var i=0;i< li.length; i++){
+//     li[i].style.backgroundColor='#f4f4f4'
+// }
+
+//  Create a new li element
+// var newLi = document.createElement("li");
+// // Set the text content of the new li element
+// newLi.textContent = "Item 4";
+// // Add the new li element to the ul element with id "myList"
+// document.getElementById("items").appendChild(newLi);
+// //
