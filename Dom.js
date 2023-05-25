24369427750944b9539e26881db08d9adafd187d
pujachitale <chitalepuja99@gmail.com>
2023-05-25 17:08:09 +0530

    first commit

--- Dom.js
+++ Dom.js
@@ -44,28 +44,47 @@
 
 
 
-var li=document.getElementsByTagName('li')
-console.log(li);
-console.log(li[1]);
-li[1].textContent='Hello 2'
-li[1].style.fontWeight='bold'
-li[1].style.backgroundColor='yellow'
+// var li=document.getElementsByTagName('li')
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello 2'
+// li[1].style.fontWeight='bold'
+// li[1].style.backgroundColor='yellow'
 
-for(var i=0;i< li.length; i++){
-    li[i].style.backgroundColor='#f4f4f4'
+// for(var i=0;i< li.length; i++){
+//     li[i].style.backgroundColor='#f4f4f4'
+// }
+
+
+// // QuerySelector
+// var header=document.querySelector('#main-header')
+// header.style.borderBottom='solid 4px black'
+
+
+var input=document.querySelector('input')
+input.value='Hello world'
+
+var submit = document.querySelector('input[type="submit"]')
+submit.value="SEND"
+
+var item= document.querySelector(".list-group-item")
+item.style.color="red"
+
+var lastItem= document.querySelector(".list-group-item:last-child")
+lastItem.style.color= "blue"
+
+var seconditem=document.querySelector(".list-group-item:nth-child(2) ")
+seconditem.style.color="coral"
+
+// queryselectorAll
+var titles=document.querySelectorAll('.title')
+console.log(titles)
+titles[0].textContent="Hello"
+
+var odd=document.querySelectorAll('li:nth-child(odd)')
+var even=document.querySelectorAll('li:nth-child(even)')
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#f4f4f4'
+    even[i].style.backgroundColor='#ccc'
 }
-
-
-// QuerySelector
-var header=document.querySelector('#main-header')
-header.style.borderBottom='solid 4px black'
-
-
-// var input=document.querySelector('input')
-// input.value='Hello world'
-
-// var submit = document.querySelector('input[type="submit"]')
-// submit.value="send"
-
-
-
